26dce330383a56e9122fcf0cfcc14849430dc2b9
GeorgeWu <stupidfive@gmail.com>
2020-06-12 23:04:50 +0800

    Change home page

--- src/ictgradschool/project/servlet/HomeServlet.java
+++ src/ictgradschool/project/servlet/HomeServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home")
+@WebServlet({"/home", "", "/"})
 public class HomeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
