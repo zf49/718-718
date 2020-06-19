8c71abe87cebf79593e8a2d377868ef56c08867c
GeorgeWu <stupidfive@gmail.com>
2020-06-19 14:43:34 +0800

    Check if password is too short when sign up

---
+++ src/ictgradschool/project/controller/exception/InvalidPasswordException.java
@@ -0,0 +1,7 @@
+package ictgradschool.project.controller.exception;
+
+public class InvalidPasswordException extends Exception {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
